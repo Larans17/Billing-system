17926a026cbee7ebf6f336aff4df9383b32ab755
Larans17 <laranssd17@gmail.com>
2025-02-11 21:10:52 +0530

    db change

--- Billing_Sys/Billing_Sys/settings.py
+++ Billing_Sys/Billing_Sys/settings.py
@@ -146,7 +146,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 API_BASE_URL = 'http://127.0.0.1:8000/api/'
 
 
-
+# -------------- Email Send ---------------
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'laranssd17@gmail.com'    
+EMAIL_HOST_PASSWORD = 'rhxzmttxprklzukt'
 
 
 
